de0c38c012e1562e2eb2e212352ce4b6e6d69d75
Alberto <albertojunior848423803@gmail.com>
2024-12-24 19:02:38 +0300

    properties working1.1

--- src/main/java/com/example/deli/StudentController.java
+++ src/main/java/com/example/deli/StudentController.java
@@ -4,14 +4,11 @@ package com.example.deli;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("api/student")
 public class StudentController {
 

b260c667d9b9a46739c0d5112f217e7b44dfe544
Alberto <albertojunior848423803@gmail.com>
2024-12-24 18:52:01 +0300

    properties working1.0

--- src/main/java/com/example/deli/DeliApplication.java
+++ src/main/java/com/example/deli/DeliApplication.java
@@ -2,9 +2,19 @@ package com.example.deli;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DeliApplication {
+
+	@GetMapping("/")
+	public String Hello(){
+		return("Hello world");
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(DeliApplication.class, args);
